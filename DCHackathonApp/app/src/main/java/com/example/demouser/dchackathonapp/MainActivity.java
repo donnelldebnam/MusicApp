5b90bbd59b3bbc6cd8308aabd143f44ab2cbd798
Donnell Debnam <donnedebnam1@aol.com>
2018-02-10 13:10:06 -0800

    working with button

--- DCHackathonApp/app/src/main/java/com/example/demouser/dchackathonapp/MainActivity.java
+++ DCHackathonApp/app/src/main/java/com/example/demouser/dchackathonapp/MainActivity.java
@@ -10,16 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-
 
     }
 
     // Button on click method
     public void buttonOnClick(View view){
         System.out.print("button clicked");
-=======
-        //comment
->>>>>>> df2eeabf3764ec8d1e34a4af77364218feab536e
+
     }
 }
